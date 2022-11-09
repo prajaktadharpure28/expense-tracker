6ae9de6ee15e8f501df765b106958fefe3fd3da2
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-11-09 18:56:26 +0530

    ref: added balance

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Balance } from "./components/Balance";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <Balance />
+        
+        </div>
+    </div>
+    
+  );
+}
+
+export default App;
